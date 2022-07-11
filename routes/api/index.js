cd21da01deac490de9cf89520ebab9cf469fae31
connor-kfitz <connorkfitzsimmons@gmail.com>
2022-07-11 17:23:23 -0400

    File/Folder Structure & Base

---
+++ routes/api/index.js
@@ -0,0 +1,11 @@
+// const router = require('express').Router();
+
+// // TODO: Add routes to files in API
+// const appRoutes = require('./');
+// const userRoutes = require('./');
+
+// // TODO: Same as above
+// router.use('/', );
+// router.use('/', );
+
+// module.exports = router;
